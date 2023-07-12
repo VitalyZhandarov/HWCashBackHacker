1e1544c46db6140e65297c4475c0fee0eadcff63
Vitaliy <Groteckon@mail.ru>
2023-07-12 23:44:05 +0300

    add1: CashbackHackServiceTest

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.service;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void calculateCashbackTest1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 700;
+
+        int actual = service.remain(amount);
+        int expected = 300;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void calculateCashbackTest2() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void calculateCashbackTest3() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 100;
+
+        int actual = service.remain(amount);
+        int expected = 900;
+
+        assertEquals(expected, actual);
+    }
+}
