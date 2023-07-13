4fbec3893007e1efd1c99a951de9954243d82f20
Vitaliy <Groteckon@mail.ru>
2023-07-13 13:40:09 +0300

    fix2: change test

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -29,10 +29,10 @@ public class CashbackHackServiceTest {
     @Test
     public void calculateCashbackTest3() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 900;
+        int amount = 1000;
 
         int actual = service.remain(amount);
-        int expected = 100;
+        int expected = 0;
 
         assertEquals(actual, expected);
     }
